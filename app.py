a531dfcc255f21264d78c95870f8a68c6ebab9d0
slolow <laszlostarost@googlemail.com>
2021-05-04 15:28:32 +0200

    Fixed Windows bug. Added javascript in upload. Added working_dependency_versions.txt

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from flask_dropzone import Dropzone
 from flask_wtf.csrf import CSRFProtect, CSRFError
 from werkzeug.urls import url_parse
 from werkzeug.utils import secure_filename
+from platform import system
 
 
 if not os.getenv('BASEDIR'):
@@ -36,12 +37,11 @@ app.config.update(
     # Flask-Dropzone config:
     SECRET_KEY=os.getenv('SECRET_KEY'),  # the secret key used to generate CSRF token
     DROPZONE_ALLOWED_FILE_CUSTOM=True,
-    #DROPZONE_ALLOWED_FILE_TYPE='image/*, .mp4, .txt, .pdf, .docx, .html, .py',
     DROPZONE_ALLOWED_FILE_TYPE='image/*, audio/*, video/*, text/*, application/*',
     DROPZONE_MAX_FILE_SIZE=3000,
     DROPZONE_MAX_FILES=30,
-    DROPZONE_PARALLEL_UPLOADS=3,  # set parallel amount
-    DROPZONE_UPLOAD_MULTIPLE=True,  # enable upload multiple
+    DROPZONE_PARALLEL_UPLOADS=1,  # set parallel amount
+    DROPZONE_UPLOAD_MULTIPLE=False,  # enable upload multiple
     DROPZONE_ENABLE_CSRF=True,  # enable CSRF protection
     DROPZONE_TIMEOUT = 3000000,
 )
@@ -67,6 +67,8 @@ def get_sub_dir_and_files(path):
 def url_to_path(path):
     """unquotes url and adds the directory seperator of operating system at the beggining of path"""
     path = urllib.parse.unquote(path)   # unquote special caracters from url like %20 --> white space
+    if system() == 'Windows':
+        return path
     return os.sep + path
 
 
@@ -112,12 +114,12 @@ def login():
             return redirect(url_for('login'))
         authenticated_user = True
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':    # An attacker could insert a URL to a malicious site in the next argument, 
+        if not next_page or url_parse(next_page).netloc != '':    # An attacker could insert a URL to a malicious site in the next argument,
             next_page = url_for('show_main_directory')            # so the application only redirects when the URL is relative,
         return redirect(next_page)                                # which ensures that the redirect stays within the same site as the application.
-    return render_template('login.html')                          # To determine if the URL is relative or absolute, I parse it with Werkzeug's url_parse() 
+    return render_template('login.html')                          # To determine if the URL is relative or absolute, I parse it with Werkzeug's url_parse()
                                                                   # function and then check if the netloc component is set or not.
-                                                                  
+
 @app.route('/logout')
 def logout():
     global authenticated_user
@@ -171,7 +173,7 @@ def delete_folder(path):
         shutil.rmtree(path)
     parent_path = os.path.dirname(path)
     return redirect(url_for('show_directory', path=parent_path))
-    
+
 
 @app.route('/upload/<path:path>', methods=['POST', 'GET'])
 def upload(path):
@@ -181,7 +183,11 @@ def upload(path):
         if request.method == 'POST':
             for key, f in request.files.items():
                 if key.startswith('file'):
-                    f.save(os.path.join(path, secure_filename(f.filename)))
+                    if not os.path.isfile(os.path.join(path, secure_filename(f.filename))):
+                        f.save(os.path.join(path, secure_filename(f.filename)))
+                    else:
+                        print('file already exists')
+                        #flash(f'{f.filename} already exists')
         return render_template('upload.html', path=path)
     return make_response(render_template('400.html', path=path), 400)
 
